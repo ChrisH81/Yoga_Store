3778cf210a1932d597f622aa54ec806338836d44
ChrisH81 <chrishurst1981@icloud.com>
2017-05-17 14:38:49 +0200

    Add Depts and Products

--- Yoga_Store.rb
+++ Yoga_Store.rb
@@ -1,1 +1,22 @@
-# Blank
+@shopping_cart = []
+
+@departments = [ :mats, :props, :clothes, :books ]
+
+@products = {
+  mats: [
+    { reference_number: 123, name: "Standard Mat", price: 10 },
+    { reference_number: 124, name: "Super Mat", price: 20 },
+  ],
+  props: [
+    { reference_number: 125, name: "Block", price: 30 },
+    { reference_number: 126, name: "Meditation Cushion", price: 30 },
+  ],
+  clothes: [
+    { reference_number: 127, name: "Elasticated Pants", price: 15 },
+    { reference_number: 128, name: "Purple Vest", price: 20 },
+  ],
+  books: [
+    { reference_number: 129, name: "Meditation Part V: The Yoga Strikes Back", price: 30 },
+    { reference_number: 130, name: "Yoga Fan Fiction", price: 20 },
+  ]
+}
